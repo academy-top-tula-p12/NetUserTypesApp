5b011f52a1d6b15c1f2eb6f412eadd95d2b17090
maxefimov <efimov.top.academy@gmail.com>
2023-12-05 21:14:14 +0300

    Добавьте файлы проекта.

---
+++ NetUserTypesApp/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NetUserTypesApp
+{
+    internal class Employee
+    {
+        public string Name;
+        public int Age;
+
+        public Employee() : this("Anonim", 0) { }
+
+        public Employee(string name) : this(name, 0) { }
+
+        public Employee(int age) : this("Anonim", age) { }
+
+        public Employee(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {this.Name}, Age: {this.Age}");
+        }
+    }
+
+    internal class User(string name, int age)
+    {
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+    }
+
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+        public Company Company;
+    }
+
+    internal class Company
+    {
+        public string Title;
+    }
+
+    internal record Account
+    {
+        public string Login;
+        public int Code;
+    }
+}
---
+++ NetUserTypesApp/Program.cs
@@ -0,0 +1,115 @@
+﻿using NetUserTypesApp;
+
+Company yandex = new() { Title = "Yandex" };
+Person tom = new() { Name = "Tom", 
+                     Age = 28, 
+                     Company = yandex };
+
+Person sam = new() { Name = "Sam", 
+                     Age = 31,
+                     Company = new() { Title = "Ozon" }
+                    };
+
+
+Account acc1 = new() { Login = "Bobby_user", Code = 123 };
+Account acc2 = new() { Login = "Bobby_user", Code = 123 };
+Account acc3 = acc1;
+
+Console.WriteLine(acc1.Equals(acc3));
+Console.WriteLine(acc1.Equals(acc2));
+
+
+void ClassOneExample()
+{
+    Employee employee = new() { Name = "Sammy", Age = 25 };
+    employee.Print();
+
+    Employee bob = new("Bob", 36);
+    Employee anonim25 = new(25);
+    Employee Tom0 = new("Tom");
+    Employee anonim0 = new();
+
+    Employee leo = new("Leo", 28);
+    Employee leo2 = leo;
+
+    //leo.Print();
+    //leo2.Print();
+
+    //leo2.Name = "Leopold";
+
+    //leo.Print();
+    //leo2.Print();
+
+    Employee leo3 = new("Leo", 28);
+
+    Console.WriteLine(leo.Equals(leo2));
+    Console.WriteLine(leo.Equals(leo3));
+
+    User user = new("User1", 55);
+    user.Print();
+}
+
+void StructExample()
+{
+    UserAccount account = new UserAccount();
+    account.name = "Bob";
+    account.age = 34;
+    account.Print();
+
+    UserAccount user = account;
+    user.name = "Sam";
+    user.Print();
+    account.Print();
+
+    UserAccount tom = new UserAccount("Tom", 25);
+    UserAccount leo = new UserAccount() { name = "Leo", age = 33 };
+
+    var joe = new UserAccount();
+    UserAccount ron = new() { name = "Ron", age = 41 };
+
+    UserAccount tim = tom with { name = "Tim" };
+
+    Console.WriteLine(account.Equals(user));
+}
+
+void EnumExample()
+{
+    WayType way;
+    way = WayType.Bottom;
+
+    way = WayType.Left;
+
+
+}
+
+enum WayType
+{
+    Left = 10,
+    Right,
+    Top = 10,
+    Bottom
+}
+
+struct UserAccount
+{
+    public string name;
+    public int age;
+
+    public UserAccount() : this("Anomim", 1)
+    {
+        //this.name = "Anonim";
+        //this.age = 1;
+    }
+
+    public UserAccount(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(name);
+    }
+}
+
